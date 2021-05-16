a1b70003f07926c7e5c0b6e7efcbd0df6f7eb2ba
Jayarani10 <83535318+Jayarani10@users.noreply.github.com>
2021-05-16 22:53:26 +0530

    Create PRIME OUTPUT.py

---
+++ PRIME OUTPUT.py
@@ -0,0 +1,36 @@
+=================== RESTART: C:/Users/ELCOT/Desktop/prime.py ===================
+
+beginning of the interval:900
+
+end of the interval:1000
+
+907
+
+911
+
+919
+
+929
+
+937
+
+941
+
+947
+
+953
+
+967
+
+971
+
+977
+
+983
+
+991
+
+997
+
+>>> 
+
